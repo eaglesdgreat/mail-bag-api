cdc1cbc3d7a469ea5e64afdaee8b3f26a225c754
Eagles Emmanuel <okponobiemmanuel@gmail.com>
2024-05-29 22:32:05 +0100

    added .env file and move emailservers credentials to it, configure email in serverin.ts file and delete the serverinfo.json file

--- src/config/ServerInfo.ts
+++ src/config/ServerInfo.ts
@@ -1,20 +1,37 @@
+import dotenv from "dotenv"
 import fs from "fs";
 import path from "path";
 
+dotenv.config();
+
 export interface IServerInfo {
   smtp: {
-    host: string
-    port: number
-    auth: { user: string; pass: string }
+    host: string | undefined
+    port: number | undefined
+    auth: { user: string | undefined; pass: string | undefined }
   };
   imap: {
-    host: string
-    port: number
-    auth: { user: string; pass: string }
+    host: string | undefined
+    port: number | undefined
+    auth: { user: string | undefined; pass: string | undefined }
   }
 }
 
-export let serverInfo: IServerInfo;
-
-const rawInfo: string = fs.readFileSync(path.join(__dirname, '../../serverInfo.json')).toString();
-serverInfo = JSON.parse(rawInfo);
+export let serverInfo: IServerInfo = {
+  imap: {
+    auth: {
+      pass: process.env.IMAP_PASS,
+      user: process.env.IMAP_USER
+    },
+    host: process.env.IMAP_HOST,
+    port: Number(process.env.IMAP_PORT)
+  },
+  smtp: {
+    auth: {
+      pass: process.env.SMTP_PASS,
+      user: process.env.SMTP_USER
+    },
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT)
+  }
+};
